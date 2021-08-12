680e14f38cc88d030a9b7b0394b892615c881ccc
Nolan Fisher <njfisher93@gmail.com>
2021-08-12 19:13:53 -0400

    Initial Commit

---
+++ test7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test7
+//
+//  Created by Nolan on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test7/test7App.swift
@@ -0,0 +1,17 @@
+//
+//  test7App.swift
+//  test7
+//
+//  Created by Nolan on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct test7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
